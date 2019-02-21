cc044e4750dafef06684994e02ea09f53a264665
vadimmos <vadimmos@ya.ru>
2019-02-21 18:26:51 +0300

    update

--- src/oda-definition-provider.ts
+++ src/oda-definition-provider.ts
@@ -1,32 +1,38 @@
 import * as vsc from 'vscode';
 import * as fs from 'fs';
-import * as pathUtil from 'path';
+import * as pu from 'path';
+const positionInFile = require('position-in-file')
+
 
 const PREFIX = 'oda-';
+const _ROOT = pu.normalize(vsc.workspace.rootPath || '/');
 
 export class OdaDefinitionProvider implements vsc.DefinitionProvider {
     async provideDefinition(document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken) {
+        const docDir = document.fileName.replace(pu.basename(document.fileName), '');
         const componnetName = getComponnetName(document, position);
         if (!componnetName) return null;
 
         const reg = new RegExp(`<link rel="import" href="(.*/(${componnetName}|${componnetName.replace(PREFIX, '')}).html)">`);
         const match = document.getText().match(reg);
-        let linkPath = match && match[1] || '';
-        if (!pathUtil.isAbsolute(linkPath)) {
-            linkPath = pathUtil.resolve(document.fileName.replace(pathUtil.basename(document.fileName), ''), linkPath);
+        let linkPath = pu.normalize(match && match[1] || '');
+
+        if (pu.isAbsolute(linkPath)) {
+            linkPath = _ROOT + linkPath;
+        } else {
+            linkPath = pu.normalize(document.fileName.replace(pu.basename(document.fileName), '') + linkPath);
         }
 
-        // const findedUri = vsc.Uri.file(linkPath);
-        // const findedDocument = await vsc.workspace.openTextDocument(findedUri);
+        const pos = positionInFile(new RegExp(`is ?: ?("|')${componnetName}("|')`), linkPath, { deep: false, ignore:[] });
 
-
+        const startPos = new vsc.Position(Number(pos.length && Object.keys(pos[0].lines)[0]) - 1, 0);
+        const endPos = new vsc.Position(Number(pos.length && Object.keys(pos[0].lines)[0])+10, 0);
 
         const componnetFilePath = await getComponentFilePath(componnetName, linkPath);
         if (!componnetFilePath) return null;
-
-        const declarationPos = new vsc.Position(0, 0);
-        if (componnetFilePath && declarationPos) {
-            return new vsc.Location(vsc.Uri.file(componnetFilePath), declarationPos);
+        const range = new vsc.Range(startPos, endPos);
+        if (componnetFilePath && range) {
+            return new vsc.Location(vsc.Uri.file(componnetFilePath), range);
         }
         return null;
     }
@@ -49,10 +55,9 @@ function getComponnetName(document: vsc.TextDocument, position: vsc.Position): s
 
 async function getComponentFilePath(componnetName: string, linkPath: string) {
     if (linkPath) {
-        const fullPath = pathUtil.resolve(vsc.workspace.rootPath || '', linkPath);
-        const check = checkFile(fullPath, componnetName);
+        const check = checkFile(linkPath, componnetName);
         if (check) {
-            return fullPath;
+            return linkPath;
         }
     }
 
@@ -73,7 +78,12 @@ async function getComponentFile(fileName: string, componnetName: string) {
     return file;
 }
 function checkFile(filePath: string, componnetName: string) {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const reg = new RegExp(`is\\s?:\\s?[\\"\\']${componnetName}[\\"\\']`, 'i');
-    return reg.test(data);
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        const reg = new RegExp(`is\\s?:\\s?[\\"\\']${componnetName}[\\"\\']`, 'i');
+        return reg.test(data);
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
 }
